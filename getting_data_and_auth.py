3769670387f475400dc83b2a7f643f774fdb909c
Andrew <Aozavyalov@protonmail.com>
2017-02-27 18:15:29 +0300

    Finished functions in getting_data_and_auth.py

--- getting_data_and_auth.py
+++ getting_data_and_auth.py
@@ -1,19 +1,23 @@
 import requests
 import json
-
-def request_to_vk():
-
-    pass
+import re
 
 
-def auth_vk():
+def request_to_vk(method, params, request=None):
+    if request:
+        request = request + '%s?%s' % (method, params)
+    else:
+        request = 'https://api.vk.com/method/%s?%s' % (method, params)
+    response = requests.get(request)
+    return response.json()
 
-    pass
 
-
-def get_vk_token():
-
-    pass
+def get_vk_token(client_id, client_secret):
+    method = 'access_token'
+    params_str = generate_params_string(
+        {'client_id': client_id, 'client_secret': client_secret, 'grant_type': 'client_credentials'})
+    token = request_to_vk(request='https://oauth.vk.com/', method=method, params=params_str)
+    return token
 
 
 def read_json(filename):
@@ -26,8 +30,25 @@ def read_json(filename):
     return json.loads(json_data)
 
 
-if __name__ == '__main__':
-    file_name = 'vk-api-schema/responses.json'
-    responses = read_json(file_name)
+def generate_params_string(params):
+    result = ''
+    for parameter in params:
+        if type(params[parameter]) == list:
+            result += '%s=' % parameter
+            for key in params[parameter]:
+                result += '%s,' % key
+            result = result[:-1] + '&'
+        else:
+            result += '%s=%s&' % (parameter, params[parameter])
+    print(result[:-1])
+    return result[:-1]
 
-    print(*responses['definitions'], sep='\n')
+
+if __name__ == '__main__':
+    app_auth_data = read_json('app_auth_data.json')
+    token = get_vk_token(app_auth_data['app_id'], app_auth_data['secure_key'])
+    print(token)
+    users_get_params = {'user_ids': ['56329050'], 'access_token': token['access_token'], 'v': '5.52',
+                        'name_case': 'Nom', 'fields': ['first_name']}
+    user = request_to_vk('users.get', generate_params_string(users_get_params))
+    print(user)
